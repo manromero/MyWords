6e1ac0747df37778a94ebdabdf6ea2913ed39faa
manromero <migu621@gmail.com>
2023-08-07 18:45:22 +0200

    install firebase auth (no configurated)

---
+++ Login.tsx
@@ -0,0 +1,41 @@
+// TODO MANROMERO this is a login example file
+// First follow the steps in https://rnfirebase.io/
+
+import React, {useState, useEffect} from 'react';
+import {View, Text} from 'react-native';
+import auth from '@react-native-firebase/auth';
+
+function App() {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <View>
+        <Text>Login</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      <Text>Welcome {user.email}</Text>
+    </View>
+  );
+}
+
+export default App;
