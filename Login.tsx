95ea6f3153ed0ba99bf5d29ba23e5eaf9c797b80
manromero <migu621@gmail.com>
2023-08-08 17:34:23 +0200

    Firebase auth with google

--- Login.tsx
+++ Login.tsx
@@ -2,39 +2,48 @@
 // First follow the steps in https://rnfirebase.io/
 
 import React, {useState, useEffect} from 'react';
-import {View, Text} from 'react-native';
+import {Button} from 'react-native';
 import auth from '@react-native-firebase/auth';
+// PARA USAR EL BOTON
+// TODO MANROMERO https://github.com/react-native-google-signin/google-signin#2-googlesigninbutton
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+
+GoogleSignin.configure({
+  webClientId:
+    '671886777936-ovfo24tj20cnrqq7ri4scunidukdvn9g.apps.googleusercontent.com',
+});
 
 function App() {
-  // Set an initializing state whilst Firebase connects
-  const [initializing, setInitializing] = useState(true);
-  const [user, setUser] = useState();
-
-  // Handle user state changes
   function onAuthStateChanged(user) {
-    setUser(user);
-    if (initializing) setInitializing(false);
+    console.log('user', user);
+    // setUser(user);
+    // if (initializing) setInitializing(false);
   }
 
   useEffect(() => {
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
     return subscriber; // unsubscribe on unmount
   }, []);
+  const onGoogleButtonPress = async () => {
+    // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
+    // Get the users ID token
+    const {idToken} = await GoogleSignin.signIn();
 
-  if (initializing) return null;
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
 
-  if (!user) {
-    return (
-      <View>
-        <Text>Login</Text>
-      </View>
-    );
-  }
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  };
 
   return (
-    <View>
-      <Text>Welcome {user.email}</Text>
-    </View>
+    <Button
+      title="Google Sign-In"
+      onPress={() =>
+        onGoogleButtonPress().then(() => console.log('Signed in with Google!'))
+      }
+    />
   );
 }
 
