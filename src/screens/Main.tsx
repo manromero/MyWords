c0c3fa7e77ccd02c450ea3c4be98da945e26fea1
manromero <migu621@gmail.com>
2023-08-09 17:24:02 +0200

    add firebase realtime database

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,5 +1,5 @@
 // vendors
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 
 // context
 import {AuthContext} from '../context';
@@ -9,9 +9,49 @@ import {Navigation} from '../routes/Navigation';
 
 // screens
 import {Login} from './Login';
+import {firebase} from '@react-native-firebase/database';
 
 export const Main = (): JSX.Element => {
   const {user} = useContext(AuthContext);
+
+  // TODO MANROMERO ELIMINAR, es solo un ejemplo
+  useEffect(() => {
+    console.log('iniciando conexión con bbdd');
+    console.log('uid', user?.uid);
+    if (!user?.uid) {
+      return;
+    }
+    console.log(
+      '-------------------------------------------------------------',
+    );
+    firebase
+      .app()
+      .database(
+        'https://mywords-4d432-default-rtdb.europe-west1.firebasedatabase.app/',
+      )
+      .ref(`/users/${user?.uid}`)
+      .push()
+      .set({
+        name: 'Ada Lovelace',
+        age: 31,
+        car: {
+          modelo: 'coche',
+          altura: 25,
+        },
+      })
+      .then(() => console.log('Data set.'))
+      .catch(e => console.log('e', e));
+    firebase
+      .app()
+      .database(
+        'https://mywords-4d432-default-rtdb.europe-west1.firebasedatabase.app/',
+      )
+      .ref(`/users/${user?.uid}`)
+      .once('value')
+      .then(snapshot => {
+        console.log('User data: ', snapshot.val());
+      });
+  }, [user?.uid]);
   if (!user) {
     return <Login />;
   }
