580806176925b0aab2a912cb229f2b55c9765832
manromero <migu621@gmail.com>
2023-08-25 18:08:04 +0200

    dark mode II

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -48,37 +48,56 @@ export const Theme = {
       DEFAULT_BORDER: '#ffffff',
     },
   },
-
-  // TODO MANROMERO los modales se ven feos
 };
 
-export const Theme2 = {
+export const ThemeDark = {
   COLORS: {
-    TEXT: {
-      PRIMARY: '#1c2025',
-      SECONDARY: '#00000099',
-      DISABLED: '#606060',
-      ACTION_PRIMARY: '#1c4dc7',
-      ACTION_DELETE: '#7e0000',
+    STATUS: {
+      DEFAULT: '#ffffff',
+      ACTIVE: '#297cda',
+      DISABLED: '#737475',
+      DANGER: '#f94f4f',
     },
     BG: {
-      PRIMARY: '#f4f6f9',
-      SECONDARY: '#ffffff',
-      DISABLED: '#e0e0e0',
-      ACTION_PRIMARY: 'transparent',
-      ACTION_DELETE: 'transparent',
-      PICKER_OPTION_SELECTED: '#dbe3fa',
-      MODAL: '#00000080',
+      PRIMARY: '#000000',
+      SECONDARY: '#161618',
+      MODAL: '#4343437f',
     },
-    ICONS: {
-      PRIMARY: '#1c2025',
-      INFO: '#1c4dc7',
-      SUCCESS: '#007e1d',
-      DISABLED: '#e0e0e0',
+    TEXT: {
+      PRIMARY: '#ffffff',
+      SECONDARY: '#aeb0b4',
     },
-    BORDER: {
-      PRIMARY: '#1c2025',
-      SECONDARY: '#606060',
+    INPUT: {
+      LABEL: '#aeb0b4',
+      COLOR: '#ffffff',
+      PLACEHOLDER: '#aeb0b4',
+      BORDER_ACTIVE: '#ffffff',
+      BORDER_INACTIVE: '#aeb0b4',
+    },
+    FOOTER: {
+      ITEM_ACTIVE: '#297cda',
+      ITEM_INACTIVE: '#737475',
+    },
+    PICKER: {
+      ITEM_BG_SELECTED: '#313133',
+      ITEM_BG_UNSELECTED: '#161618',
+      ITEM_LABEL_SELECTED: '#ffffff',
+      ITEM_LABEL_UNSELECTED: '#ffffff',
+    },
+    ACTION_BUTTON: {
+      PRIMARY_ACTIVE_BG: 'transparent',
+      PRIMARY_ACTIVE_LABEL: '#297cda',
+      PRIMARY_DISABLED_BG: 'transparent',
+      PRIMARY_DISABLED_LABEL: '#737475',
+      DELETE_ACTIVE_BG: 'transparent',
+      DELETE_ACTIVE_LABEL: '#f94f4f',
+      DELETE_DISABLED_BG: 'transparent',
+      DELETE_DISABLED_LABEL: '#737475',
+    },
+    TAG: {
+      DEFAULT_LABEL: '#ffffff',
+      DEFAULT_BG: '#000000',
+      DEFAULT_BORDER: '#ffffff',
     },
   },
 };
